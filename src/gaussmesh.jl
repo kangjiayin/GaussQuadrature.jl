4b53e6cff18b17f257f8f35ae08d756cbfa3b7fb
kangjiayin <891124092@qq.com>
2021-08-15 19:19:01 +0800

    新更改

---
+++ src/gaussmesh.jl
@@ -0,0 +1,34 @@
+# This flie is made for getting nodes and weight of gauss quadrature
+
+using FastGaussQuadrature
+
+
+
+# function gausslegendremesh
+# from a to b
+# make step nodes
+# n-different kinds of mesh
+#
+function gausslegendremesh(a::Float64,b::Float64,step::Int,n::Int)
+    x=Array{Float64}(undef,step,1)
+    w=Array{Float64}(undef,step,1)
+    x,w= gausslegendre(step)
+    if n==1
+        #放缩系数k
+        k1= (b-a)/2
+        k2= (a+b)/2
+        x=k2 .+(k1 .*x)
+        for i in eachindex(x)
+            result+=f(x[i])*w[i]
+        end
+        result*=(b-a)/2
+    elseif n==2
+        a=sqrt(a)
+        b=sqrt(b)
+        x=a*b*(b/a).^x
+        for i in eachindex(x)
+            w[i]*=x[i]*(log(b)-log(a))
+        end
+    end
+    x,w
+end
