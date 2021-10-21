f7c6fc7521e9636dea4bc05d05ee146ada8625dc
Your Name <you@example.com>
2021-10-21 15:57:39 +0800

    add int128

--- src/Gaussquad.jl
+++ src/Gaussquad.jl
@@ -1,15 +1,12 @@
 module Gaussquad
 using FastGaussQuadrature, LinearAlgebra
-
 export gausslegendreint
 # export gausschebyshevint
 export gausschebyshevint64
 export gausslegendreint64
-
-
+export gausslegendreint128
 export gausslegendremesh
 
 include("./gaussint.jl")
 include("./gaussmesh.jl")
-
 end # module
