745ddc51881f04fcce21a686899fea865e5d2352
Your Name <you@example.com>
2021-11-13 15:59:16 +0800

    same

--- src/Gaussquad.jl
+++ src/Gaussquad.jl
@@ -6,7 +6,7 @@ export gausschebyshevint64
 export gausslegendreint64
 export gausslegendreint128
 export gausslegendremesh
-
+export gausslegendreint512
 include("./gaussint.jl")
 include("./gaussmesh.jl")
 end # module
